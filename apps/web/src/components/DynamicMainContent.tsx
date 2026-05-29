8fc9c56e21238f0236a019068390928c790f83a9
kilicseyit <seyit.kilic6@gmail.com>
2026-05-29 03:37:19 +0300

    fix: hide sidebar burger button when collapsed and show on hover expand, revert collapsed width to w-16

--- apps/web/src/components/DynamicMainContent.tsx
+++ apps/web/src/components/DynamicMainContent.tsx
@@ -9,7 +9,7 @@ export default function DynamicMainContent({ children }: { children: React.React
   return (
     <div
       className={`flex flex-1 flex-col min-w-0 transition-all duration-300 ${
-        isCollapsed ? 'lg:pl-20' : 'lg:pl-64'
+        isCollapsed ? 'lg:pl-16' : 'lg:pl-64'
       }`}
     >
       {children}
